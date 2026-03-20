2a5799496170a86751ab9555c60c8ba25427125d
akedify <akash@edifytech.us>
2026-03-20 19:33:47 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medico_chatbot",
+    version="0.1.0",
+    author="AK",
+    author_email="akashrosa6@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
